8b1029724c281ac2e4e2bec69791829e10b18d81
SanjanaRao204 <sanjanarao858@gmail.com>
2026-02-26 05:45:49 +0000

    New Files

---
+++ main.py
@@ -0,0 +1,36 @@
+from groq import Groq
+
+# Initialize Groq client
+client = Groq(api_key="")
+
+def generate_explanation(topic, style):
+    system_prompt = """
+    You are an expert teacher.
+    Always explain clearly and fully stay in the selected character style.
+    """
+
+    user_prompt = f"Explain {topic} in {style} style."
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        temperature=0.8
+    )
+
+    return response.choices[0].message.content
+
+
+# ---- Main Program ----
+print("🎭 Prompt-Based Style Explainer")
+print("Choose style: Shakespeare / Pirate / Bandit")
+
+topic = input("Enter topic: ")
+style = input("Enter style: ")
+
+output = generate_explanation(topic, style)
+
+print("\n--- AI Explanation ---\n")
+print(output)
